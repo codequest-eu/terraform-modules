150a9d441848e2105086b09d80b1090051011432
Marek Skrajnowski <marek@codequest.com>
2019-04-17 19:20:28 +0200

    PoC: worker instance

--- ecs/example/main.tf
+++ ecs/example/main.tf
@@ -2,6 +2,8 @@ locals {
   project       = "terraform-modules-ecs"
   project_index = 0
   environment   = "example"
+
+  name = "${local.project}-${local.environment}"
 }
 
 provider "aws" {
@@ -22,4 +24,22 @@ module "worker_role" {
 
   project     = "${local.project}"
   environment = "${local.environment}"
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "${local.project}-${local.environment}"
+}
+
+module "worker" {
+  source = "../worker"
+
+  name        = "${local.name}-worker"
+  project     = "${local.project}"
+  environment = "${local.environment}"
+
+  instance_type     = "t3.micro"
+  instance_profile  = "${module.worker_role.profile_name}"
+  subnet_id         = "${module.network.private_subnet_ids[0]}"
+  security_group_id = "${module.network.workers_security_group_id}"
+  cluster_name      = "${aws_ecs_cluster.cluster.name}"
 }
---
+++ ecs/worker/main.tf
@@ -0,0 +1,38 @@
+locals {
+  default_tags = {
+    Name        = "${var.name}"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+
+  tags = "${merge(local.default_tags, var.tags)}"
+}
+
+data "aws_ami" "ecs_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/templates/user_data.sh")}"
+
+  vars {
+    cluster_name = "${var.cluster_name}"
+  }
+}
+
+resource "aws_instance" "worker" {
+  ami                  = "${data.aws_ami.ecs_amazon_linux.id}"
+  instance_type        = "${var.instance_type}"
+  subnet_id            = "${var.subnet_id}"
+  security_groups      = ["${var.security_group_id}"]
+  iam_instance_profile = "${var.instance_profile}"
+  user_data            = "${data.template_file.user_data.rendered}"
+
+  tags = "${local.tags}"
+}
