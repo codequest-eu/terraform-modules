d072f14f982f20764fdb641b7ac6176d36f710be
Marek Skrajnowski <marek@codequest.com>
2019-04-25 17:39:21 +0200

    elasticache/redis module

---
+++ elasticache/redis/example/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_availability_zones" "all" {}
+
+data "aws_subnet" "a" {
+  vpc_id            = "${data.aws_vpc.default.id}"
+  availability_zone = "${data.aws_availability_zones.all.names[0]}"
+  default_for_az    = true
+}
+
+data "aws_subnet" "b" {
+  vpc_id            = "${data.aws_vpc.default.id}"
+  availability_zone = "${data.aws_availability_zones.all.names[1]}"
+  default_for_az    = true
+}
+
+module "redis" {
+  source = ".."
+
+  id          = "tfm-redis-example"
+  project     = "terraform-modules-redis"
+  environment = "example"
+
+  vpc_id     = "${data.aws_vpc.default.id}"
+  subnet_ids = ["${data.aws_subnet.a.id}"]
+
+  instance_type  = "cache.t2.micro"
+  instance_count = 1
+}
+
+output "redis_url" {
+  value     = "${module.redis.url}"
+  sensitive = true
+}
+
+output "redis_urls" {
+  value     = "${module.redis.urls}"
+  sensitive = true
+}
