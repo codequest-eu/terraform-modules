0a48b88cd7d79d87615f2bc78a55add839eebccb
Marek Skrajnowski <marek@codequest.com>
2019-08-06 19:10:09 +0200

    ecs/network: Typed variables

--- ecs/network/variables.tf
+++ ecs/network/variables.tf
@@ -1,37 +1,45 @@
 variable "project" {
   description = "Kebab-cased project name"
+  type        = string
 }
 
 variable "environment" {
   description = "Kebab-cased environment name, eg. development, staging, production."
+  type        = string
 }
 
 variable "tags" {
   description = "Tags to add to resources that support them"
+  type        = map(string)
   default     = {}
 }
 
 variable "project_index" {
   description = "Unique project number in 0-255 range which will be used to build the VPC CIDR block: 10.{project_index}.0.0/16"
+  type        = number
 }
 
 variable "availability_zones_count" {
   description = "Number of availability zones the network should span"
+  type        = number
   default     = 2
 }
 
 variable "bastion_ingress_cidr_blocks" {
   description = "CIDR blocks from where you should be able to access the bastion host"
+  type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
 variable "nat_instance" {
   description = "Use NAT instances instead of NAT gateways."
+  type        = bool
   default     = false
 }
 
 variable "nat_instance_type" {
   description = "EC2 instance type to use to create a NAT instance."
+  type        = string
   default     = "t3.nano"
 }
 
