6d5148817ec322d11bbba4a39d4768084d149729
Marek Skrajnowski <marek@codequest.com>
2019-08-06 19:09:10 +0200

    ecs/repository: Typed variables

--- ecs/repository/variables.tf
+++ ecs/repository/variables.tf
@@ -1,9 +1,11 @@
 variable "project" {
   description = "Kebab-cased project name"
+  type        = string
 }
 
 variable "tags" {
   description = "Tags to add to resources that support them"
+  type        = map(string)
   default     = {}
 }
 
