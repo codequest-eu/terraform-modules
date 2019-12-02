42ee4e9581ad99c649acca4aa2d963c6287cb220
Marek Skrajnowski <marek@codequest.com>
2019-12-02 18:22:35 +0100

    fix(ssl/acm): is empty tuple errors on terraform >=0.12.11

--- ssl/acm/outputs.tf
+++ ssl/acm/outputs.tf
@@ -10,5 +10,8 @@ output "arn" {
 
 output "validated_arn" {
   description = "ACM certificate ARN, once it's validated"
-  value       = var.create && aws_acm_certificate_validation.cert[0].id != null ? aws_acm_certificate.cert[0].arn : null
+
+  # aws_acm_certificate_validation.cert != null will always be true
+  # and is only used to force a dependency on validation
+  value = var.create && aws_acm_certificate_validation.cert != null ? aws_acm_certificate.cert[0].arn : null
 }
