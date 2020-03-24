6cf6ecc8877eca7818be32c80fc86d6c2ac5274c
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2020-03-24 18:32:25 +0100

    refactor(ssl/acm): use depends_on for validated_arn output (#32)
    Closes #30

--- ssl/acm/outputs.tf
+++ ssl/acm/outputs.tf
@@ -10,8 +10,6 @@ output "arn" {
 
 output "validated_arn" {
   description = "ACM certificate ARN, once it's validated"
-
-  # aws_acm_certificate_validation.cert != null will always be true
-  # and is only used to force a dependency on validation
-  value = var.create && aws_acm_certificate_validation.cert != null ? aws_acm_certificate.cert[0].arn : null
+  value       = var.create ? aws_acm_certificate.cert[0].arn : null
+  depends_on  = [aws_acm_certificate_validation.cert]
 }
