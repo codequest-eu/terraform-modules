450968b93cae206a361631501d272e6dce98e362
Marek Skrajnowski <marek@codequest.com>
2020-03-25 12:48:37 +0100

    feat(ecs/host_group): added memory and disk usage reporting

--- ecs/host_group/templates/user_data.sh
+++ ecs/host_group/templates/user_data.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 echo ECS_CLUSTER=${cluster_name} >> /etc/ecs/ecs.config
+
+# Setup memory and disk usage monitoring
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/mon-scripts.html
+
+yum install -y \
+  unzip \
+  curl \
+  perl-Switch \
+  perl-DateTime \
+  perl-Sys-Syslog \
+  perl-LWP-Protocol-https \
+  perl-Digest-SHA.x86_64
+
+cd /opt
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip && \
+rm CloudWatchMonitoringScripts-1.2.2.zip
+
+cat >/etc/cron.d/ec2_metrics <<EOF
+*/5 * * * * ec2-user /opt/aws-scripts-mon/mon-put-instance-data.pl --from-cron --mem-util --mem-used --mem-avail --swap-util --swap-used
+*/5 * * * * ec2-user /opt/aws-scripts-mon/mon-put-instance-data.pl --from-cron --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail
+EOF
