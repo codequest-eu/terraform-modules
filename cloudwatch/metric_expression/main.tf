b56bd8168b648c8df457142ba7fa92a1603ea7fb
Marek Skrajnowski <marek@codequest.com>
2020-04-22 19:10:26 +0200

    chore(cloudwatch): removed todos that related to cloudwatch/alarms

--- cloudwatch/metric_expression/main.tf
+++ cloudwatch/metric_expression/main.tf
@@ -6,13 +6,4 @@ locals {
   ]))
 
   id = "e_${local.vars_hash}"
-
-  # TODO: move to cloudwatch/alarm
-  # https://www.terraform.io/docs/providers/aws/r/cloudwatch_metric_alarm.html
-  # alarm_metric_query = { expression = var.expression }
-  # alarm_metric = merge(
-  #   { metric_query = local.alarm_metric_query },
-  #   var.id != null ? { id = var.id } : {},
-  #   var.label != null ? { label = var.label } : {}
-  # )
 }
