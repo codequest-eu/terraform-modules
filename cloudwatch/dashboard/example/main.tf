af324f45fd682cf2bb8817df3ac434da8d81a6f7
Marek Skrajnowski <marek@codequest.com>
2020-04-23 13:37:27 +0200

    docs(cloudwatch/dashboard): restructured examples to focus on layout

--- cloudwatch/dashboard/example/main.tf
+++ cloudwatch/dashboard/example/main.tf
@@ -2,107 +2,81 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-data "aws_ami" "amazon_linux2_minimal" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-minimal-hvm-2.0.20200406.0-x86_64-ebs"]
-  }
-}
-
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.amazon_linux2_minimal.id
-  instance_type = "t3a.nano"
-  monitoring    = true
-
-  tags = {
-    Name = "terraform-modules-cloudwatch-dashboard-example"
-  }
-}
-
 module "metric_cpu_utilization" {
   source = "./../../metric"
 
   namespace = "AWS/EC2"
   name      = "CPUUtilization"
-  dimensions = {
-    InstanceId = aws_instance.instance.id
-  }
 }
 
-module "widget_cpu_utilization" {
-  source = "./../../metric_widget"
-
-  title        = "${aws_instance.instance.id} CPU utilization"
-  dimensions   = [12, 6]
-  left_metrics = [module.metric_cpu_utilization]
-  left_range   = [0, null]
-}
-
-module "metric_cpu_credit_balance" {
+module "metric_network_in" {
   source = "./../../metric"
 
   namespace = "AWS/EC2"
-  name      = "CPUCreditBalance"
-  dimensions = {
-    InstanceId = aws_instance.instance.id
-  }
+  name      = "NetworkIn"
+  stat      = "Sum"
 }
 
-module "expression_cpu_credit_balance_rate" {
-  source = "./../../metric_expression"
+module "metric_network_out" {
+  source = "./../../metric"
 
-  expression = "RATE(${module.metric_cpu_credit_balance.id})"
-  label      = "Rate of CPUCreditBalance"
+  namespace = "AWS/EC2"
+  name      = "NetworkOut"
+  stat      = "Sum"
 }
 
-module "widget_cpu_credit_balance" {
+# Simple dashboard with default layout
+module "widget_simple_cpu_utilization" {
   source = "./../../metric_widget"
 
-  title         = "${aws_instance.instance.id} CPU credit balance"
-  dimensions    = [12, 6]
-  left_metrics  = [module.metric_cpu_credit_balance]
-  left_range    = [0, null]
-  right_metrics = [module.expression_cpu_credit_balance_rate]
+  title        = "CPU Utilization"
+  left_metrics = [module.metric_cpu_utilization]
 }
 
-module "widget_current_cpu_credit_balance" {
+module "widget_simple_network_traffic" {
   source = "./../../metric_widget"
 
-  title      = "${aws_instance.instance.id} CPU"
-  dimensions = [6, 3]
-  view       = "singleValue"
-  left_metrics = [
-    module.metric_cpu_utilization,
-    module.metric_cpu_credit_balance
+  title        = "Network traffic"
+  stacked      = true
+  left_metrics = [module.metric_network_in, module.metric_network_out]
+}
+
+module "dashboard_simple" {
+  source = "./.."
+
+  name = "terraform-modules-cloudwatch-dashboard-example-simple"
+  widgets = [
+    module.widget_simple_cpu_utilization,
+    module.widget_simple_network_traffic,
   ]
 }
 
-module "alarm_cpu_utilization" {
-  source = "./../../alarm"
+# Dashboard with custom layout
+module "widget_custom_cpu_utilization" {
+  source = "./../../metric_widget"
 
-  name      = "terraform-modules-cloudwatch-dashboard-example-cpu-utilization"
-  condition = [module.metric_cpu_utilization.id, ">", 80]
-  metrics   = [module.metric_cpu_utilization]
+  title        = "CPU Utilization"
+  left_metrics = [module.metric_cpu_utilization]
+  position     = [0, 0]
+  dimensions   = [24, 6]
 }
 
-module "widget_cpu_utilization_alarm" {
-  source = "./../../alarm_widget"
+module "widget_custom_network_traffic" {
+  source = "./../../metric_widget"
 
-  title     = "CPU utilization alarm"
-  alarm_arn = module.alarm_cpu_utilization.arn
+  title        = "Network traffic"
+  stacked      = true
+  left_metrics = [module.metric_network_in, module.metric_network_out]
+  position     = [0, 6]
+  dimensions   = [24, 6]
 }
 
-module "dashboard" {
+module "dashboard_custom" {
   source = "./.."
 
-  name = "terraform-modules-cloudwatch-dashboard-example"
+  name = "terraform-modules-cloudwatch-dashboard-example-custom"
   widgets = [
-    module.widget_cpu_utilization,
-    module.widget_cpu_credit_balance,
-    module.widget_current_cpu_credit_balance,
-    module.widget_cpu_utilization_alarm,
+    module.widget_custom_cpu_utilization,
+    module.widget_custom_network_traffic,
   ]
 }
