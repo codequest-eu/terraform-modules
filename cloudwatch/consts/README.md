95a5d202ffa4f0bc18e5ce53e597d21f056bfbb8
Marek Skrajnowski <marek@codequest.com>
2020-04-24 16:01:22 +0200

    feat(cloudwatch): added consts module with cloudwatch's default color palette

---
+++ cloudwatch/consts/README.md
@@ -0,0 +1,17 @@
+# cloudwatch/consts
+
+Common cloudwatch-related constants, like the color palette cloudwatch uses by default.
+
+<!-- bin/docs -->
+
+## Versions
+
+| Provider | Requirements |
+|-|-|
+| terraform | `>= 0.12` |
+
+## Outputs
+
+* `colors`
+
+    Cloudwatch default color palette
---
+++ cloudwatch/consts/outputs.tf
@@ -0,0 +1,26 @@
+output "colors" {
+  description = "Cloudwatch default color palette"
+  value = {
+    blue   = "#1f77b4"
+    orange = "#ff7f0e"
+    green  = "#2ca02c"
+    red    = "#d62728"
+    purple = "#9467bd"
+    brown  = "#8c564b"
+    pink   = "#e377c2"
+    grey   = "#7f7f7f"
+    olive  = "#bcbd22"
+    cyan   = "#17becf"
+
+    light_blue   = "#aec7e8"
+    light_orange = "#ffbb78"
+    light_green  = "#98df8a"
+    light_red    = "#ff9896"
+    light_purple = "#c5b0d5"
+    light_brown  = "#c49c94"
+    light_pink   = "#f7b6d2"
+    light_grey   = "#c7c7c7"
+    light_olive  = "#dbdb8d"
+    light_cyan   = "#9edae5"
+  }
+}
