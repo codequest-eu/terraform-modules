02f079e43334e6c9d982d9746f1b01d01c9f7d9c
Marek Skrajnowski <marek@codequest.com>
2020-04-28 13:36:24 +0200

    refactor(ecs/host_group): extracted group_dimensions local

--- ecs/host_group/metrics.tf
+++ ecs/host_group/metrics.tf
@@ -20,124 +20,103 @@ module "cloudwatch_consts" {
 
 locals {
   colors = module.cloudwatch_consts.colors
+  group_dimensions = {
+    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
+  }
 }
 
 module "metric_min_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupMinSize"
-  label     = "Minimum instances"
-  color     = local.colors.light_grey
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupMinSize"
+  label      = "Minimum instances"
+  color      = local.colors.light_grey
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_max_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupMaxSize"
-  label     = "Maximum instances"
-  color     = local.colors.light_grey
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupMaxSize"
+  label      = "Maximum instances"
+  color      = local.colors.light_grey
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_desired_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupDesiredCapacity"
-  label     = "Desired instances"
-  color     = local.colors.grey
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupDesiredCapacity"
+  label      = "Desired instances"
+  color      = local.colors.grey
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupTotalInstances"
-  label     = "Total instances"
-  color     = local.colors.green
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupTotalInstances"
+  label      = "Total instances"
+  color      = local.colors.green
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_in_service_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupInServiceInstances"
-  label     = "In service instances"
-  color     = local.colors.green
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupInServiceInstances"
+  label      = "In service instances"
+  color      = local.colors.green
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_pending_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupPendingInstances"
-  label     = "Pending instances"
-  color     = local.colors.orange
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupPendingInstances"
+  label      = "Pending instances"
+  color      = local.colors.orange
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_standby_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupStandbyInstances"
-  label     = "Standby instances"
-  color     = local.colors.light_green
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupStandbyInstances"
+  label      = "Standby instances"
+  color      = local.colors.light_green
+  period     = 60
+  stat       = "Average"
 }
 
 module "metric_terminating_instances" {
   source = "./../../cloudwatch/metric"
 
-  namespace = "AWS/AutoScaling"
-  name      = "GroupTerminatingInstances"
-  label     = "Terminating instances"
-  color     = local.colors.red
-  period    = 60
-  stat      = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = var.create ? aws_autoscaling_group.hosts[0].name : ""
-  }
+  namespace  = "AWS/AutoScaling"
+  dimensions = local.group_dimensions
+  name       = "GroupTerminatingInstances"
+  label      = "Terminating instances"
+  color      = local.colors.red
+  period     = 60
+  stat       = "Average"
 }
