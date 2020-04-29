5c5cbaed3b3a36635a7abeac21a57234833b993e
Marek Skrajnowski <marek@codequest.com>
2020-04-29 14:04:03 +0200

    fix(cloudwatch/metric/many): variables should be optional

--- cloudwatch/metric/many/variables.tf
+++ cloudwatch/metric/many/variables.tf
@@ -1,9 +1,11 @@
 variable "vars" {
   description = "List of [cloudwatch/metric](./..) variables"
   type        = any
+  default     = []
 }
 
 variable "vars_map" {
   description = "Map of [cloudwatch/metric](./..) variables"
   type        = any
+  default     = {}
 }
