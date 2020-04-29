742eec15088f4f403356dd28c88bd2fd38fff847
Marek Skrajnowski <marek@codequest.com>
2020-04-29 13:38:31 +0200

    feat(cloudwatch/metric_expression): added many module

---
+++ cloudwatch/metric_expression/many/variables.tf
@@ -0,0 +1,9 @@
+variable "vars" {
+  description = "List of [cloudwatch/metric_expression](./..) variables"
+  type        = any
+}
+
+variable "vars_map" {
+  description = "Map of [cloudwatch/metric_expression](./..) variables"
+  type        = any
+}
