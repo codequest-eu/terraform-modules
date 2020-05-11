e07c4c8126f13ba66ef0f71951635ebd3edcda2e
Marek Skrajnowski <marek@codequest.com>
2020-05-11 15:57:12 +0200

    Revert "temp: introduced a bug"
    This reverts commit 37aa4e9d9bae9600eae406401f3695b3a3c37f69.

--- ssl/acm/variables.tf
+++ ssl/acm/variables.tf
@@ -4,6 +4,11 @@ variable "create" {
   type        = bool
 }
 
+variable "domains" {
+  description = "Certificate domains, have to be in one Route53 hosted zone."
+  type        = list(string)
+}
+
 variable "hosted_zone_id" {
   description = "Route53 hosted zone id for ACM domain ownership validation"
   type        = string
