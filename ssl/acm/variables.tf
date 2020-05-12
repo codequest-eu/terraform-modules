f067a462e3934d7968cd1b9898ec62a6512dbac5
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2020-05-12 09:31:28 +0200

    feat(ssl/acm): added validation variables and wildcard support (#79)
    * feat(ssl/acm): added validate and create_validation_records variables for finer control over validation
    
    * feat(ssl/acm): wildcard certificate support

--- ssl/acm/variables.tf
+++ ssl/acm/variables.tf
@@ -20,3 +20,22 @@ variable "tags" {
   default     = {}
 }
 
+variable "validate" {
+  description = "Whether to wait for certificate validation"
+  type        = bool
+  default     = true
+}
+
+variable "create_validation_records" {
+  description = <<EOF
+Whether to create DNS records for validation.
+
+    When creating certificates for the same domain in different regions,
+    ACM will request the same DNS records for validation, which will make
+    terraform try to create the same records twice and fail.
+    Use this variable to make sure only one of the modules creates
+    the validation records.
+EOF
+  type        = bool
+  default     = true
+}
