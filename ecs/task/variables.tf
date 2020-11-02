b3f466f758ce1f182ac1c9df8cb0cf8fda2c3e42
Marek Skrajnowski <marek@codequest.com>
2020-11-02 13:11:06 +0100

    docs(ecs/task): fix log_retention docs indentation

--- ecs/task/variables.tf
+++ ecs/task/variables.tf
@@ -41,10 +41,10 @@ variable "container" {
 
 variable "log_retention" {
   description = <<EOF
-    Log retention in days.
+Log retention in days.
 
     Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, and 0.
-    If you select 0, the events in the log group are always retained and never expire."
+    If you select 0, the events in the log group are always retained and never expire.
   EOF
 
   type    = number
