93beb6f56e11f53324ec40167a48b954a0ce718c
Marek Skrajnowski <marek@codequest.com>
2021-01-11 12:20:37 +0100

    docs(cloudfront): more complex example

--- cloudfront/example/main.tf
+++ cloudfront/example/main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# 1. Setup a cloudfront origin, eg. an S3 bucket with some objects
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "terraform-modules-example-cloudfront"
 }
 
-resource "aws_s3_bucket_object" "website" {
+resource "aws_s3_bucket_object" "index" {
   bucket       = aws_s3_bucket.bucket.bucket
   key          = "index.html"
   content      = "<h1>${aws_s3_bucket.bucket.bucket}</h1>"
@@ -14,15 +16,12 @@ resource "aws_s3_bucket_object" "website" {
   metadata     = { "cache-control" = "max-age=0, must-revalidate" }
 }
 
-module "bucket_access_document" {
-  source               = "./../origin/s3/bucket_policy_document"
-  bucket_arn           = aws_s3_bucket.bucket.arn
-  access_identity_arns = [module.example.access_identity_arn]
-}
-
-resource "aws_s3_bucket_policy" "bucket" {
-  bucket = aws_s3_bucket.bucket.bucket
-  policy = module.bucket_access_document.json
+resource "aws_s3_bucket_object" "assetlinks" {
+  bucket       = aws_s3_bucket.bucket.bucket
+  key          = ".well-known/assetlinks.json"
+  content      = jsonencode([])
+  content_type = "application/json"
+  metadata     = { "cache-control" = "max-age=0, must-revalidate" }
 }
 
 module "origin_bucket" {
@@ -30,19 +29,54 @@ module "origin_bucket" {
   bucket = aws_s3_bucket.bucket.bucket
 }
 
+# 2. Create cloudfront behaviors, which specify how paths are handled and cached, eg.:
+#    - forward all requests to the bucket
+#    - require basic auth for all paths except `./well-known/*`
 
-module "behavior_forward_to_bucket" {
-  source    = "./../behavior"
+module "lambda_basic_auth" {
+  source      = "./../lambda/basic_auth"
+  name        = "terraform-modules-example-cloudfront-basic-auth"
+  credentials = "terraform-modules:example"
+}
+
+module "behavior_default" {
+  source = "./../behavior"
+
+  origin_id             = "bucket"
+  viewer_request_lambda = module.lambda_basic_auth
+}
+
+module "behavior_wellknown" {
+  source = "./../behavior"
+
+  path      = "/.well-known/*"
   origin_id = "bucket"
 }
 
-module "example" {
+# 3. Create the cloudfront distribution using the above setup
+
+module "cloudfront" {
   source = "./.."
 
-  s3_origins       = { bucket = module.origin_bucket }
-  default_behavior = module.behavior_forward_to_bucket
+  s3_origins = { bucket = module.origin_bucket }
+
+  default_behavior = module.behavior_default
+  behaviors        = { wellknown = module.behavior_wellknown }
 }
 
-output "example" {
-  value = module.example
+# 4. Give cloudfront access to the bucket
+
+module "bucket_access_document" {
+  source               = "./../origin/s3/bucket_policy_document"
+  bucket_arn           = aws_s3_bucket.bucket.arn
+  access_identity_arns = [module.cloudfront.access_identity_arn]
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.bucket
+  policy = module.bucket_access_document.json
+}
+
+output "cloudfront" {
+  value = module.cloudfront
 }
