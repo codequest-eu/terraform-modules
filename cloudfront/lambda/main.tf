9cf7dddb0c73a7491ada6afef8da6292679b5079
Marek Skrajnowski <marek@codequest.com>
2021-01-11 11:55:02 +0100

    fix(cloudfront/lambda): missing policy

--- cloudfront/lambda/main.tf
+++ cloudfront/lambda/main.tf
@@ -11,15 +11,24 @@ data "aws_iam_policy_document" "cloudfront_assume_role" {
   }
 }
 
+resource "aws_iam_policy" "cloudfront_assume_role" {
+  count  = var.create ? 1 : 0
+  name   = "${var.name}-cloudfront-assume-role"
+  policy = data.aws_iam_policy_document.cloudfront_assume_role[0].json
+}
+
 module "lambda" {
   source = "./../../lambda"
   create = var.create
 
-  function_name = var.name
-  tags          = var.tags
+  name = var.name
+  tags = var.tags
 
-  files       = { "index.js" = var.code }
-  handler     = var.handler
-  runtime     = var.runtime
-  policy_arns = data.aws_iam_policy_document.cloudfront_assume_role[0].arn
+  files   = { "index.js" = var.code }
+  handler = var.handler
+  runtime = var.runtime
+
+  policy_arns = {
+    cloudfront_assume_role = aws_iam_policy.cloudfront_assume_role[0].arn
+  }
 }
