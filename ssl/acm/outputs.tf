c0d1d22409a0c01a60c672cb1d9740abe010c57c
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2021-01-20 11:48:42 +0100

    feat(ssl/acm)!: support for aws provider 3.0 and manual validation (#99)
    * docs(ssl/acm): wrong example wildcard_certificate_arn output value
    
    * feat(ssl/acm)!: support and require AWS provider 3.x
    
    * feat(ssl/acm): support creating validation records manually
    
    * feat(ssl/acm): made hosted_zone_id optional

--- ssl/acm/outputs.tf
+++ ssl/acm/outputs.tf
@@ -13,3 +13,8 @@ output "validated_arn" {
   value       = var.create && var.validate ? aws_acm_certificate.cert[0].arn : null
   depends_on  = [aws_acm_certificate_validation.cert]
 }
+
+output "validation_records" {
+  description = "DNS validation records, in cases where you want to manually create them"
+  value       = local.validation_records
+}
