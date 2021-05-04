36238b27e9b152232d22e4aaf2a9e6abf9505feb
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2021-05-04 10:17:30 +0200

    feat(cloudfront): added lambda which adds response headers (#115)
    * fix(cloudfront/lambda): cloudfront allows max 30s timeout
    
    * feat(cloudfront): added response_headers lambda
    
    * fix(cloudfront/lambda/response_headers): path matching
    
    * docs(cloudfront/lambda/response_headers): updated docs
    
    * docs(cloudfront/lambda/response_headers): invalidation example
    
    * fix(cloudfront/lambda/response_headers): shouldn't require rules
    
    * docs(cloudfront/lambda/response_headers): added a link to micromatch features
    
    * feat(cloudfront): added cloudfront/invalidation module
    
    * refactor(cloudfront/invalidation): use aws cloudfront wait

---
+++ cloudfront/lambda/response_headers/main.tf
@@ -0,0 +1,32 @@
+locals {
+  global_rule = {
+    path         = "**"
+    content_type = "**"
+    headers      = var.global
+  }
+
+  rules = concat([local.global_rule], var.rules)
+  rules_json = jsonencode([for rule in local.rules : {
+    path : rule.path,
+    contentType : rule.content_type,
+    headers : rule.headers
+  }])
+}
+
+module "lambda" {
+  source = "./../../../lambda"
+  create = var.create
+
+  name = var.name
+  tags = var.tags
+
+  files = {
+    "index.js"   = file("${path.module}/dist/index.js")
+    "rules.json" = local.rules_json,
+  }
+
+  handler                = "index.handler"
+  runtime                = "nodejs12.x"
+  timeout                = 30
+  assume_role_principals = ["edgelambda.amazonaws.com"]
+}
