89ef25f0abadfaa323b3fd4398c3db085c2408ae
Marek Skrajnowski <marek@codequest.com>
2021-06-02 15:59:58 +0200

    feat: allow terraform 0.13

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 # Used in CI to download plugins once
 terraform {
-  required_version = ">= 0.12, <0.13"
+  required_version = ">= 0.12, <0.14"
 
   # Make sure all providers needed by any module are listed
   required_providers {
