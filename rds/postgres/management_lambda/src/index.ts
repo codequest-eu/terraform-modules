6aed7f9ac52d3a297b703399f96445e704598d32
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2021-06-16 15:02:44 +0200

    feat(rds/postgres): pass database url to management lambda via SSM Parameter Store (#120)
    * feat(rds/postgres): updated postgres version
    
    * feat(rds/postgres): added SSM parameters with the master db password and url
    
    * refactor(rds/postgres/management_lambda): build using @vercel/ncc
    
    * docs(rds/postgres): readme update
    
    * feat(rds/postgres/management_lambda): pass database url using SSM
    
    * feat(rds/postgres): pass db url to management lambda using SSM
    
    * feat(lambda): added role_name and role_arn outputs
    
    * fix(rds/postgres): management lambda can't live in a public subnet
    
    * feat(rds/postgres): only create SSM parameters when management_lambda is created
    
    * docs(rds/postgres): updated readme

--- rds/postgres/management_lambda/src/index.ts
+++ rds/postgres/management_lambda/src/index.ts
@@ -2,14 +2,18 @@ import { ClientConfig } from "pg"
 import Client from "pg/lib/client"
 import pgConnectionString from "pg-connection-string"
 
+import SSMClient from "aws-sdk/clients/ssm"
+
 interface Event {
   queries: string[]
   database?: string
 }
 
+const ssmClient = new SSMClient({})
+
 export async function handler({ database, queries }: Event) {
   const clientConfig = pgConnectionString.parse(
-    process.env.DATABASE_URL!,
+    await getDatabaseUrl(),
   ) as ClientConfig
 
   if (database) {
@@ -29,4 +33,31 @@ export async function handler({ database, queries }: Event) {
     console.log(`Closing connection to the '${clientConfig.database}' database`)
     await client.end()
   }
+}
+
+async function getDatabaseUrl() {
+  if (process.env.DATABASE_URL) {
+    return process.env.DATABASE_URL
+  }
+
+  if (process.env.DATABASE_URL_PARAM) {
+    const paramName = process.env.DATABASE_URL_PARAM
+
+    const result = await ssmClient
+      .getParameter({
+        Name: paramName,
+        WithDecryption: true,
+      })
+      .promise()
+
+    const paramValue = result.Parameter?.Value
+
+    if (!paramValue) {
+      throw new Error(`Failed to fetch ${paramName} from SSM`)
+    }
+
+    return paramValue
+  }
+
+  throw new Error("Database credentials not provided")
 }
