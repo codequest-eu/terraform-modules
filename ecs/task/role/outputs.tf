1ba6b1ca278e4d33439101646d9ae7dfd9a14743
Marek Skrajnowski <marek@codequest.com>
2021-07-05 14:09:09 +0200

    feat(ecs/task/role): ensure that the role is not used before policies are attached

--- ecs/task/role/outputs.tf
+++ ecs/task/role/outputs.tf
@@ -1,9 +1,23 @@
 output "name" {
   description = "Role name"
   value       = var.create ? aws_iam_role.this[0].name : ""
+
+  # Make sure the role is not used before policies are attached
+  depends_on = [
+    aws_iam_role_policy.inline,
+    aws_iam_role_policy_attachment.attachment,
+    aws_iam_role_policy_attachment.execution,
+  ]
 }
 
 output "arn" {
   description = "Role ARN"
   value       = var.create ? aws_iam_role.this[0].arn : ""
+
+  # Make sure the role is not used before policies are attached
+  depends_on = [
+    aws_iam_role_policy.inline,
+    aws_iam_role_policy_attachment.attachment,
+    aws_iam_role_policy_attachment.execution,
+  ]
 }
