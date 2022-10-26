7806fbc6e2731876063a498486895e9f84c07958
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2022-10-26 14:14:24 +0200

    fix(ecs/task/role): role could be used before policies have been assigned (#143)

--- ecs/task/role/outputs.tf
+++ ecs/task/role/outputs.tf
@@ -1,9 +1,21 @@
 output "name" {
+  depends_on = [
+    aws_iam_role_policy.inline,
+    aws_iam_role_policy_attachment.attachment,
+    aws_iam_role_policy_attachment.execution,
+  ]
+
   description = "Role name"
   value       = var.create ? aws_iam_role.this[0].name : ""
 }
 
 output "arn" {
+  depends_on = [
+    aws_iam_role_policy.inline,
+    aws_iam_role_policy_attachment.attachment,
+    aws_iam_role_policy_attachment.execution,
+  ]
+
   description = "Role ARN"
   value       = var.create ? aws_iam_role.this[0].arn : ""
 }
