91c0102503a0e107dc9b04d68deaf9e259720afe
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2023-03-24 12:06:19 +0100

    feat: enable default server side encryption for S3 buckets (#147)
    AWS has switched on server side encryption by default and for existing buckets

--- redirect/main.tf
+++ redirect/main.tf
@@ -17,6 +17,14 @@ resource "aws_s3_bucket" "redirect" {
       }]
     EOF
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "redirect" {
